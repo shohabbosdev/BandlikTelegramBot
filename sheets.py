b7698e35009ba348946c14b9089fc4afd34138b0
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2025-08-23 00:39:59 +0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -3,22 +3,26 @@ import gspread
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-from config import SHEET_ID, WORKSHEET_TITLE
-
 # .env faylini yuklaymiz
 load_dotenv()
 
-# Secret fayl yo‘li Render secrets orqali yoki .env orqali
+# Google Sheets API scope
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
+# Secret fayl yo‘li
 creds_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-creds = Credentials.from_service_account_file(creds_path, scopes=SCOPES)
+if not creds_path or not os.path.exists(creds_path):
+    raise FileNotFoundError("Google credentials.json fayli topilmadi!")
 
+creds = Credentials.from_service_account_file(creds_path, scopes=SCOPES)
 GC = gspread.authorize(creds)
 
 # .env fayldan olingan Sheet nomlari
 SHEET_ID = os.environ.get("SHEET_ID")
 WORKSHEET_TITLE = os.environ.get("WORKSHEET_TITLE")
+
+if not SHEET_ID or not WORKSHEET_TITLE:
+    raise ValueError(".env faylida SHEET_ID yoki WORKSHEET_TITLE topilmadi!")
 
 # Worksheetni ochish
 WS = GC.open_by_key(SHEET_ID).worksheet(WORKSHEET_TITLE)
