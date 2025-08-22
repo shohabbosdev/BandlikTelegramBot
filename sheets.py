9ed320dc7edd202a9dfaa1da219923d282829621
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2025-08-23 00:18:21 +0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,20 +1,13 @@
+import os
 import gspread
 from google.oauth2.service_account import Credentials
-from config import SHEET_ID, WORKSHEET_TITLE
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive",
-]
+# JSON faylni env variable orqali o‘qiymiz
+creds_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+if not creds_path:
+    raise RuntimeError("GOOGLE_APPLICATION_CREDENTIALS yo'q!")
 
-# credentials.json loyihangiz ildizida bo'lishi kerak
-CREDS = Credentials.from_service_account_file("credentials.json", scopes=SCOPE)
-GC = gspread.authorize(CREDS)
-WS = GC.open_by_key(SHEET_ID).worksheet(WORKSHEET_TITLE)
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file(creds_path, scopes=SCOPES)
 
-def load_rows():
-    """
-    Varaqdagi barcha qatorlarni oladi (2D ro'yxat).
-    0-qatorda header bo'ladi.
-    """
-    return WS.get_all_values()
+GC = gspread.authorize(creds)
