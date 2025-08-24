4ad2570682a4685c203402c6b0eff50381b7adeb
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2025-08-24 12:33:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
+import asyncio
 from flask import Flask, request
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, filters
 from handlers import start, stat, search, grafik, inline_pagination_handler
 from config import TOKEN
-import asyncio
 
 # Flask ilovasi
 app = Flask(__name__)
@@ -24,8 +24,11 @@ telegram_app.add_handler(CallbackQueryHandler(inline_pagination_handler, pattern
 def webhook():
     data = request.get_json(force=True)
     update = Update.de_json(data, telegram_app.bot)
-    asyncio.run(telegram_app.process_update(update))
+    # mavjud event loopdan foydalanamiz
+    loop = asyncio.get_event_loop()
+    loop.create_task(telegram_app.process_update(update))
     return "OK", 200
+
 
 # Oddiy test route
 @app.get("/")
@@ -33,5 +36,10 @@ def home():
     return "Bot is running!", 200
 
 if __name__ == "__main__":
+    # Applicationni initialize qilamiz
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(telegram_app.initialize())
+    loop.run_until_complete(telegram_app.start())
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
